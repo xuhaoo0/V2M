71bb0c8d2874fdcc102f88087281da1e582f875b
xuhao <526358612@qq.com>
2026-09-16 21:49:58 +0800

    9.16.22

--- gvhmr.py
+++ gvhmr.py
@@ -1,5 +1,6 @@
 '''
-跑一条数据
+重建一条数据
+
 args:
 - video，例如a/b/c.mp4
 - output_root，例如gvhmr_out/，结果输出在gvhmr_out/c
@@ -7,14 +8,25 @@ args:
 
 import os
 import subprocess
+import sys
+from pathlib import Path
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"  # gpu
+v2m_root = Path(__file__).resolve().parent
+gvhmr_root = v2m_root / "GVHMR"
 
-subprocess.run([
-    "python",
-    "GVHMR/tools/demo/demo.py",
-    "--video",
-    "/home/xuhao/data/sam3-main/dance/single/KG_kg_iconx/person1.mp4",  # 输入视频
-    "--output_root",
-    "gvhmr_out",  # 输出目录
-], check=True)
+env = os.environ.copy()
+env["CUDA_VISIBLE_DEVICES"] = "1"  # gpu
+
+subprocess.run(
+    [
+        sys.executable,
+        "tools/demo/demo.py",
+        "--video",
+        "/home/xuhao/data/sam3-main/dance/single/KG_kg_iconx/person1.mp4",  # 输入视频
+        "--output_root",
+        str(v2m_root / "gvhmr_out"),  # 输出目录
+    ],
+    cwd=gvhmr_root,
+    env=env,
+    check=True,
+)
--- split.py
+++ split.py
@@ -1,12 +1,19 @@
 '''
 切一条视频
+内容类似：
+def func1()...
+def func2()...
+
+if main:
+  ...
 
 args：
 - input_video，例如a/b/c.mp4
-- output_dir，例如split_out/，然后就输出每个片段
-  - split_out/c/split/c_001.mp4
-  - split_out/c/split/c_002.mp4
+- output_dir，例如split_out/，保存每个片段
+  - split_out/c/c_001/c_001.mp4
+  - split_out/c/c_002/c_002.mp4
 
 切分方法：
-- 参考宗宇的代码
+- 目标：？
+- 参考宗宇的代码？
 '''
